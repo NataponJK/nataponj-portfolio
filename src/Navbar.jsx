db696dbb829b1f1624359f659ada96291baa3403
NataponJ <nataponj@gmail.com>
2024-02-26 15:28:03 +0700

    change resume modal to google doc viewer

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
-
   return (
     <div className="flex justify-between items-center gap-3 mb-10 font-light">
       <div>
@@ -29,53 +25,22 @@ const Navbar = () => {
               <img className="h-[30px] rounded" src="github.png" />
             </li>
           </a>
-          <button
-            title="Resume"
-            className="flex items-center"
-            onClick={() => setOpen(true)}
-          >
+          <button title="Resume" className="flex items-center">
             <li>
-              <img
-                className="h-[30px] rounded animate-bounce animate-duration-[2500ms]"
-                src="resume.png"
-              />
+              <a
+                href="https://docs.google.com/document/d/14wUDa6ORkdmREwkAbnIXltXQ4YCZ5yb1_WEhXCwMwo8/edit?usp=sharing"
+                target="_blank"
+              >
+                <img
+                  className="h-[30px] rounded animate-bounce animate-duration-[2500ms]"
+                  src="resume.png"
+                />
+              </a>
             </li>
           </button>
         </ul>
         <p className="flex items-center">NataponJ@gmail.com</p>
       </div>
-      {open && (
-        <div className="animate-fade animate-ease-out fixed top-0 left-0 w-full h-dvh z-10">
-          <div className="flex justify-around mx-3 mt-3 mb-2">
-            <div className="w-[30px] h-[30px]"></div>
-            <div>
-              <a href="https://drive.google.com/file/d/1eiewyEHoE8YRtQPLjhLssC6b0pEOAFpH/view?usp=sharing">
-                <img
-                  className="w-[30px] h-[30px]"
-                  src="download.png"
-                  title="Download"
-                />
-              </a>
-            </div>
-            <button onClick={() => setOpen(false)} className="px-6">
-              <img
-                className="w-[30px] h-[30px]"
-                src="close.png"
-                alt="close"
-                title="Close"
-              />
-              <div></div>
-            </button>
-          </div>
-          <div className="w-full h-full item-center overflow-scroll">
-            <img
-              className="w-auto mx-auto px-6"
-              src="resume.jpg"
-              alt="resume"
-            />
-          </div>
-        </div>
-      )}
     </div>
   );
 };
